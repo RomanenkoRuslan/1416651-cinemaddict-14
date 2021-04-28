19946821d5c21fabb19470775fdf3bd346ac3134
Ruslan Romanenko <ruslan.roman19@mail.ru>
2021-04-29 00:03:50 +0300

    Добавление функций replace, remove

--- src/util/render.js
+++ src/util/render.js
@@ -1,3 +1,5 @@
+import Abstract from '../view/abstract.js';
+
 //Создает элемент
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -8,13 +10,21 @@ const RenderPosition = {
   BEFOREEND: 'beforeend',
 };
 
-const renderElement = (container, element, place) => {
+const renderElement = (container, child, place) => {
+  if (container instanceof Abstract) {
+    container = container.getElement();
+  }
+
+  if (child instanceof Abstract) {
+    child = child.getElement();
+  }
+
   switch (place) {
     case RenderPosition.AFTERBEGIN:
-      container.prepend(element);
+      container.prepend(child);
       break;
     case RenderPosition.BEFOREEND:
-      container.append(element);
+      container.append(child);
       break;
   }
 };
@@ -25,4 +35,31 @@ const createElement = (template) => {
   return newElement.firstChild;
 };
 
-export {renderTemplate, renderElement, createElement, RenderPosition};
+const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+
+const remove = (component) => {
+  if (!(component instanceof Abstract)) {
+    throw new Error('Can remove only components');
+  }
+
+  component.getElement().remove();
+  component.removeElement();
+};
+
+export {renderTemplate, renderElement, createElement, RenderPosition, replace, remove};
